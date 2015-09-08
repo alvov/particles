a74f84f3e2e290c8ba79f8f6e3150fd181ae17e0
Artyom Lvov <alvov@avito.ru>
2015-09-08 21:29:49 +0300

    added third dimension

--- script.js
+++ script.js
@@ -1,57 +1,47 @@
-(function(){
+(function() {
     'use strict';
-    
-    const UNIVERSE_SIZE = 400,
-        COLLISION_DISTANCE = 3,
+
+    const COLLISION_DISTANCE = 3,
         EXPLOSION_TIME = 2000,
         MIN_MASS = 5,
         MAX_MASS = 90,
         G = 0.00667384; // IRL: G / Math.pow(10, 10)
-    
-    var Space,
-        space,
-        Particle;
-    
-    // Space constructor
-    Space = function(){
-        var spaceElement;
-        this.params = {
-            window: {
-                width: window.innerWidth,
-                height: window.innerHeight
-            }
-        };
-        this.params.window.center = {
-            x: Math.round(this.params.window.width / 2),
-            y: Math.round(this.params.window.height / 2)
-        };  
+
+    /**
+     * Space constructor
+     * @constructor
+     */
+    var Space = function() {
+        this.universeSize = Math.min(window.innerWidth, window.innerHeight);
+        this.universeCenter = [this.universeSize / 2, this.universeSize / 2, 0];
         this.particles = [];
 
-        spaceElement = document.createElement('div');
+        var spaceElement = document.createElement('div');
         spaceElement.className = 'space';
         this.element = document.createElement('div');
-        this.element.style.width = this.params.window.width + 'px';
-        this.element.style.height = this.params.window.height + 'px';
-        this.element.className = 'canvas';
+        this.element.style.width = this.universeSize + 'px';
+        this.element.style.height = this.universeSize + 'px';
+        this.element.className = 'universe';
         spaceElement.appendChild(this.element);
         document.body.appendChild(spaceElement);
-
-        this.render();
-        this.step();
     };
-    // Generates particle with given parameters
+
+    /**
+     * Generates particle with given parameters
+     * @param {Object} params
+     */
     Space.prototype.generateParticle = function(params) {
-        var newParticle = new Particle(utils.extend(params, {
+        var newParticle = new Particle(Object.assign(params, {
             parent: this
         }));
         this.element.appendChild(newParticle.element);
         this.particles.push(newParticle);
-        if (this.particles.length === 1) {
-            this.render();
-            this.step();
-        }
     };
-    // Destroys given particle
+
+    /**
+     * Destroys given particle
+     * @param {Particle} particle
+     */
     Space.prototype.destroyParticle = function(particle){
         this.particles.every(function(curParticle, i) {
             if (curParticle === particle) {
@@ -62,18 +52,28 @@
             return true;
         }.bind(this));
     };
-    // Returns sum of vector-like objects
-    Space.prototype.sumProjections = function(obj1, obj2) {
-        return {
-            x: obj1.x + obj2.x,
-            y: obj1.y + obj2.y
-        };
+
+    /**
+     * Returns distance between points
+     * @param {Array} vector1
+     * @param {Array} vector2
+     * @returns {number}
+     */
+    Space.prototype.getDistance = function(vector1, vector2) {
+        return Math.sqrt(
+            vector1
+                .map(function(value, i) {
+                    return Math.pow(value - vector2[i], 2);
+                })
+                .reduce(function(prev, current) {
+                    return prev + current;
+                })
+        );
     };
-    // Returns distance between points
-    Space.prototype.getDistance = function(obj1, obj2) {
-        return Math.sqrt(Math.pow(obj1.x - obj2.x, 2) + Math.pow(obj1.y - obj2.y, 2));
-    };
-    // Renders particles and launches cycle
+
+    /**
+     * Renders particles and launches cycle
+     */
     Space.prototype.render = function() {
         this.particles.forEach(function(curParticle) {
             curParticle.render();
@@ -82,14 +82,16 @@
             window.requestAnimationFrame(this.render.bind(this));
         }
     };
-    // Calculates new particles position/state
+
+    /**
+     * Calculates new particles position/state
+     */
     Space.prototype.step = function() {
         // count speed
         this.particles.forEach(function(curParticle) {
             var forcesList = [];
             var collided = false;
             this.particles.forEach(function(otherParticle) {
-                var distance;
                 if (
                     otherParticle === curParticle ||
                     curParticle.state.value === 'destroyed' ||
@@ -97,7 +99,7 @@
                 ) {
                     return;
                 }
-                distance = curParticle.parent.getDistance(curParticle, otherParticle);
+                var distance = this.getDistance(curParticle.pos, otherParticle.pos);
                 if (distance <= COLLISION_DISTANCE) {
                     collided = true;
                 }
@@ -111,7 +113,7 @@
                         forcesList.push(curParticle.getForce(otherParticle));
                     }
                 }
-            });
+            }, this);
             if (curParticle.state.value === 'destroyed') {
                 return;
             }
@@ -119,14 +121,13 @@
                 curParticle.state.set('default');
             }
             if (forcesList.length) {
-                var sumForce = forcesList.reduce(this.sumProjections),
-                    boost = {
-                        x: sumForce.x / curParticle.mass,
-                        y: sumForce.y / curParticle.mass
-                    };
-                curParticle.speed = this.sumProjections(curParticle.speed, boost);
+                var sumForce = forcesList.reduce(utils.vectors.add);
+                var boost = sumForce.map(function(value) {
+                    return value / curParticle.mass;
+                });
+                curParticle.speed = utils.vectors.add(curParticle.speed, boost);
             }
-        }.bind(this));
+        }, this);
         // apply speed
         this.particles.forEach(function(curParticle) {
             curParticle.move();
@@ -141,16 +142,21 @@
         }
     };
 
-    // Particle constructor
-    Particle = function(params) {
+    /**
+     * Particle constructor
+     * @param {Object} params
+     * @constructor
+     */
+    var Particle = function(params) {
         this.parent = params.parent;
         this.state = utils.observableValue('newBorn'); // ['newBorn', 'default', exploding', 'destroyed']
         this.timeouts = {};
-        this.x = params.x;
-        this.y = params.y;
-        this._oldX = null;
-        this._oldY = null;
-        this.speed = this.getProjectionValue(params.speed, utils.angle.toRad(params.dir));
+        this.pos = params.pos;
+        this._oldPos = [];
+        this.speed = params.dir.map(function(value) {
+            return value * params.speed
+        });
+
         this.mass = params.mass;
         this._oldMass = null;
         this.element = document.createElement('p');
@@ -159,10 +165,14 @@
         }.bind(this));
         this.render();
     };
-    // Collides particle with another given particle
+
+    /**
+     * Collides particle with another given particle
+     * @param {Particle} otherParticle
+     */
     Particle.prototype.collision = function(otherParticle) {
-        var carrier,
-            provider;
+        var carrier;
+        var provider;
         // particle with bigger mass wins
         if (this.mass > otherParticle.mass) {
             carrier = this;
@@ -171,73 +181,97 @@
             carrier = otherParticle;
             provider = this;
         }
-        
-        carrier.speed.x = (this.speed.x * this.mass + otherParticle.speed.x * otherParticle.mass) / (this.mass + otherParticle.mass);
-        carrier.speed.y = (this.speed.y * this.mass + otherParticle.speed.y * otherParticle.mass) / (this.mass + otherParticle.mass);
-        
+
+        var commonMass = carrier.mass + provider.mass;
+        carrier.speed = carrier.speed.map(function(value, i) {
+            return (value * carrier.mass + provider.speed[i] * provider.mass) / commonMass;
+        });
+
         carrier._oldMass = carrier.mass;
-        carrier.mass += provider.mass;
+        carrier.mass = commonMass;
         provider.destroy();
     };
-    // Returns value of gravity force relative to given particle
+
+    /**
+     * Returns value of gravity force relative to given particle
+     * @param {Particle} otherParticle
+     * @returns {Array}
+     */
     Particle.prototype.getForce = function(otherParticle) {
-        var distance = this.parent.getDistance(this, otherParticle),
-            force,
-            dir;
+        var distance = this.parent.getDistance(this.pos, otherParticle.pos);
         if (!distance) {
-            force = 0;
+            return [0, 0, 0];
         } else {
-            force = G * this.mass * otherParticle.mass / Math.pow(distance, 2);
-            dir = Math.atan((this.y - otherParticle.y) / (this.x - otherParticle.x));
-            if (this.x > otherParticle.x) {
-                dir += utils.angle.PI;
-            }
+            var force = G * this.mass * otherParticle.mass / Math.pow(distance, 2);
+            var dir = this.pos.map(function(value, i) {
+                return otherParticle.pos[i] - value;
+            });
+            dir = this.getNormalizedVector(dir);
+
+            return dir.map(function(value) {
+                return value * force;
+            });
         }
-        return this.getProjectionValue(force, dir);
     };
-    // Calculates new particle's position
+
+    /**
+     * Calculates particle's new position
+     */
     Particle.prototype.move = function() {
-        this._oldX = this.x;
-        this._oldY = this.y;
-        this.x += this.speed.x;
-        this.y += this.speed.y;
-        // limit particles spread by UNIVERSE_SIZE
-        if (this.parent.getDistance(this, this.parent.params.window.center) >= UNIVERSE_SIZE) {
-            ['x', 'y'].forEach(function(axis) {
-                if ((this[axis] - this.parent.params.window.center[axis]) * this.speed[axis] > 0) {
-                    if (this.speed[axis] > 0) {
-                        this.speed[axis] = Math.max(0, this.speed[axis] - Math.max(this.speed[axis] * 0.01, 0.001));
-                    } else {
-                        this.speed[axis] = Math.min(0, this.speed[axis] - Math.min(this.speed[axis] * 0.01, -0.001));
-                    }
+        this._oldPos = this.pos.slice();
+        this.pos = this.pos.map(function(value, i) {
+            return value + this.speed[i];
+        }, this);
+        // limit particles spread by universe size
+        if (this.parent.getDistance(this.pos, this.parent.universeCenter) >= this.parent.universeSize / 2) {
+            this.pos.forEach(function(value, i) {
+                if ((value - this.parent.universeCenter[i]) * this.speed[i] > 0) {
+                    this.speed[i] = Math.max(
+                        0,
+                        this.speed[i] - Math.max(this.speed[i] * 0.01, this.speed[i] > 0 ? 0.001 : -0.001)
+                    );
                 }
-            }.bind(this));
+            }, this);
         }
     };
-    // Returns section's projection on a given axis
-    Particle.prototype.getProjectionValue = function(value, angleRad, axis) {
-        var result = {};
-        if (!axis || axis === 'x') {
-            result.x = value * Math.cos(angleRad);
-        }
-        if (!axis || axis === 'y') {
-            result.y = value * Math.sin(angleRad);
-        }
-        return result;
+
+    /**
+     * Returns normalized vector
+     * @param {Array} vector
+     * @returns {Array}
+     */
+    Particle.prototype.getNormalizedVector = function(vector) {
+        var vectorSize = Math.sqrt(vector.reduce(function(result, value) {
+            return result + Math.pow(value, 2);
+        }, 0));
+        return vector.map(function(value) {
+            return value / vectorSize;
+        });
     };
-    // Renders current particle's position and size
+
+    /**
+     * Renders current particle's position and size
+     */
     Particle.prototype.render = function() {
         if (this._oldMass !== this.mass) {
-            this.element.style.boxShadow = '0 0 3px ' + Math.min(4, Math.ceil(this.mass * 0.05, 0)) + 'px';
+            this.element.style.boxShadow = '0 0 3px ' + Math.min(4, Math.ceil(this.mass * 0.05)) + 'px';
         }
         if (
-            utils.round(this._oldX, 6) !== utils.round(this.x, 6) ||
-            utils.round(this._oldY, 6) !== utils.round(this.y, 6)
+            this.pos.some(function(value, i) {
+                return utils.round(value, 6) !== utils.round(this._oldPos[i], 6);
+            }, this)
         ) {
-            this.element.style.transform = 'translate(' + utils.round(this.x, 0) + 'px,' + utils.round(this.y, 0) + 'px)';
+            this.element.style.transform = 'translate3d(' +
+                this.pos.map(function(value) {
+                    return utils.round(value, 0) + 'px';
+                }).join(',') +
+            ')';
         }
     };
-    // Destroys particle
+
+    /**
+     * Destroys particle
+     */
     Particle.prototype.destroy = function() {
         Object.keys(this.timeouts).forEach(function(key){
             if (this.timeouts[key]) {
@@ -248,50 +282,62 @@
         this.parent.destroyParticle(this);
         this.state.set('destroyed');
     };
-    // Sets particle's delayed wxplosion
+
+    /**
+     * Sets particle's delayed explosion
+     * @param {number} delay
+     */
     Particle.prototype.delayedExplosion = function(delay){
         delay = delay || EXPLOSION_TIME;
         this.timeouts.explosion = setTimeout(this.explode.bind(this), delay);
         this.state.set('exploding');
     };
-    // Explodes particle into smaller parts and destroys it
+
+    /**
+     * Explodes particle into smaller parts and destroys it
+     */
     Particle.prototype.explode = function(){
         this.splitMass().forEach(function(mass){
             this.parent.generateParticle({
-                x: this.x,
-                y: this.y,
+                pos: this.pos.slice(),
                 mass: mass,
-                speed: utils.random.number(4, 50) / 100,
-                dir: utils.random.number(0, 360)
+                speed: utils.random.number(4, 100) / 100,
+                dir: this.getNormalizedVector([
+                    utils.random.number(-100, 100),
+                    utils.random.number(-100, 100),
+                    utils.random.number(-100, 100)
+                ])
             });
-        }.bind(this));
+        }, this);
         this.destroy();
     };
-    // Returns a random array of masses, which sum is equal to the particles mass
-    Particle.prototype.splitMass = function(){
-        var masses = [],
-            particlesCount = Math.floor(this.mass / MIN_MASS),
-            remainingMass = this.mass % MIN_MASS,
-            randomPick;
+
+    /**
+     * Returns a random array of masses, which sum is equal to the particles mass
+     * @returns {Array}
+     */
+    Particle.prototype.splitMass = function() {
+        var masses = [];
+        var particlesCount = Math.floor(this.mass / MIN_MASS);
+        var remainingMass = this.mass % MIN_MASS;
         for (var i = 0; i < particlesCount; i++) {
             masses.push(MIN_MASS);
         }
         while (remainingMass) {
-            randomPick = utils.random.number(1, remainingMass);
+            var randomPick = utils.random.number(1, remainingMass);
             masses[utils.random.number(0, particlesCount - 1)] += randomPick;
             remainingMass -= randomPick;
         }
         return masses;
     };
-    
-    space = new Space();
-    
+
+    var space = new Space();
     space.generateParticle({
-        x: space.params.window.center.x,
-        y: space.params.window.center.y,
-        dir: 0,
+        dir: [0, 0, 0],
+        pos: space.universeCenter.slice(),
         speed: 0,
         mass: 800 // some kind of a big bang
     });
-    
+    space.render();
+    space.step();
 })();
