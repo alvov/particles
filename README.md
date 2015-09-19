a0bf6a28ebdfcea801ed2994046219a0c1b7ec25
Artyom Lvov <alvov@avito.ru>
2015-09-19 19:53:57 +0300

    added readme file

---
+++ README.md
@@ -0,0 +1,8 @@
+# Particles
+
+Model description:
+
+1. All particles have a "mass" and hence gravitate to each other.
+2. Being close enough particles can collide and become a bigger particle (with cumulative mass).
+3. When the mass of the particle reaches the critical value, the particle explodes into smaller particles.
+4. The universe is finite, so that the particles cannot leave its boundaries.
--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ var buffer = require('vinyl-buffer');
 gulp.task('js', function() {
     var b = browserify()
         .add('./node_modules/tsify/node_modules/typescript/bin/lib.es6.d.ts')
-        .add('./js/script.ts')
+        .add('./js/main.ts')
         .plugin('tsify', { target: 'es5' });
 
     return b.bundle()
         .pipe(source('bundle.js'))
         .pipe(buffer())
-        .pipe(gulp.dest('./js/'));
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', ['js'], function() {
